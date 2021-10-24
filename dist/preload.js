8c648d869e8c520dedea706b0760ad3e878ed3b4
Erik Stenmark <erik.stenmark@gmai.com>
2021-10-24 16:03:28 +0300

    removed dist

--- dist/preload.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld('electron', {
-    toggleFullScreen: function () { return electron_1.ipcRenderer.invoke('toggle-full-screen'); }
-});
